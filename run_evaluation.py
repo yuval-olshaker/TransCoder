2b123cfd7f7084b42bbce62404cb5e66a2bcad5f
yuvalol <yuvalolshaker@gmail.com>
2020-09-14 19:36:01 +0300

    print not exists tests as well. fix bugs - now we can run

--- run_evaluation.py
+++ run_evaluation.py
@@ -41,9 +41,9 @@ def add_code_to_tests(lang, lines):
 
             titles.append([lines[0], len(relevant_lines)])  # add the title and lines amount
         else:
-            not_exists.append(lines[0])
+            not_exists.append(lines[0] + '\n')
         lines = lines[i + 1:]
-    return titles
+    return titles, not_exists
 
 
 # run the tests of python language (translated from java)
@@ -55,11 +55,12 @@ def run_python_tests(titles):
             os.system('python3 ' + dir_path + title[0] + '.py > ' + dir_path + 'temp.txt')  # run the python test
             with open(dir_path + 'temp.txt') as result:  # take the result we printed
                 res = result.read()[:-1].split()  # no EOL, split by ' '
-                results.append([title[0], title[1], res[0], res[1]])
+                results.append([title[0], title[1], res[-2], res[-1]])
         except:
             results.append([title[0], title[1], -1, -1])
         os.system('rm ' + dir_path + 'temp.txt')
     return results
+
 
 # run the tests of java language (translated from python)
 def run_java_tests(titles):
@@ -67,16 +68,24 @@ def run_java_tests(titles):
     dir_path = '/mnt/c/TransCoder/outputs/java/changed/'
     for title in titles:
         try:
-            os.system('javac' + dir_path + title[0] + '.java') # compile the java test
-            os.system('cd ' + dir_path) # go into the dir because java is annoying
-            os.system('java ' + title[0] + ' > temp.txt') # run the java test
+            os.system('javac' + dir_path + title[0] + '.java')  # compile the java test
+            os.system('cd ' + dir_path)  # go into the dir because java is annoying
+            os.system('java ' + title[0] + ' > temp.txt')  # run the java test
             with open(dir_path + 'temp.txt') as result:  # take the result we printed
                 res = result.read()[:-1].split()  # no EOL, split by ' '
-                results.append([title[0], title[1], res[0], res[1]])
+                results.append([title[0], title[1], res[-2], res[-1]])
         except:
             results.append([title[0], title[1], -1, -1])
         os.system('rm ' + dir_path + 'temp.txt')
     return results
+
+
+# order the results so we will print them
+def order_results(results):
+    return list(map(
+        lambda res: (res[0] + ' lines: ' + str(res[1]) + ' successes: ' + str(res[2]) + ' tests: ' + str(
+            res[3]) + '\n'), results))
+
 
 if __name__ == '__main__':
     print_time()
@@ -86,16 +95,23 @@ if __name__ == '__main__':
     python_lines = read_translated('/mnt/c/TransCoder/outputs/translated_python.py')
 
     # put it out
-    python_titles = add_code_to_tests('python', python_lines)
-    java_titles = add_code_to_tests('java', java_lines)
+    python_titles, python_not_exists = add_code_to_tests('python', python_lines)
+    java_titles, java_not_exists = add_code_to_tests('java', java_lines)
 
     # run the tests
-    results = run_python_tests(python_titles)
+    python_results = run_python_tests(python_titles)
+    java_results = run_java_tests(java_titles)
 
-    results = map(
-        lambda res: (res[0] + ' num lines: ' + res[1] + ' num successes: ' + res[2] + ' total_tests: ' + res[3]),
-        results)
+    python_results = order_results(python_results)
+    java_results = order_results(java_results)
 
-    with open('/mnt/c/TransCoder/outputs/tests_run_check.txt') as tests_run_check:
-        tests_run_check.writelines(results)
+    with open('/mnt/c/TransCoder/outputs/tests_run_check_python.txt', 'w') as f:
+        f.writelines(python_results)
+    with open('/mnt/c/TransCoder/outputs/not_exists_python.txt', 'w') as f:
+        f.writelines(python_not_exists)
+    with open('/mnt/c/TransCoder/outputs/tests_run_check_java.txt', 'w') as f:
+        f.writelines(java_results)
+    with open('/mnt/c/TransCoder/outputs/not_exists_java.txt', 'w') as f:
+        f.writelines(java_not_exists)
+
     print_time()
