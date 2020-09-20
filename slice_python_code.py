f0ffa1c15e213316957ae79d1d6d0c5fce6f4da4
yuvalol <yuvalolshaker@gmail.com>
2020-09-20 19:45:47 +0300

    start the trivial integration process

--- slice_python_code.py
+++ slice_python_code.py
@@ -5,6 +5,10 @@ INDENTATION = 4
 BRANCH_STATEMENTS = ['if', 'else', 'elif', 'for', 'while']
 REGULAR = 'regular'
 EASY_CODE = '        a = 5\n' # an easy code inside branch. with indentation (twice. 1 for function and 1 for branch)
+OPEN_SPECIAL_BRACKET = '{'
+CLOSE_SPECIAL_BRACKET = '}'
+OPEN_BRACKET = '('
+CLOSE_BRACKET = ')'
 
 def print_time(to_print):
     now = datetime.now()
@@ -69,6 +73,50 @@ def runable_slices(slices, whole_function):
         slices[i][1] = function_declaration + '    ' + slices[i][1]
     return slices
 
+# finds the index for the end of the line that we are interested in
+# it is at the end of the condition. because we not have the inside of the branch
+def find_end_of_declaration(interesting_code):
+    brackets = 1
+    i = 2
+    while brackets != 0 and i < len(interesting_code):
+        word = interesting_code[i]
+        if word == OPEN_BRACKET:
+            brackets += 1
+        elif word == CLOSE_BRACKET:
+            brackets -= 1
+        i += 1
+    return i
+
+# strips java line - and order it to be with no indentations
+def strip_line(line):
+    splitted_line = line.split('\n') # splitted the code to lines.
+    # first line in function declaration - we do not need it
+    # second line its our code
+    interesting_code = splitted_line[1].strip().split(' ') # take the line, no indentation, split with ' '
+    # if starts branch - we take the branch declaration and add '{', we remove the a=5 part.
+    if interesting_code[0] in BRANCH_STATEMENTS:
+        # cut the line and add the opening of branch in java
+        to_return = ' '.join(interesting_code).replace('a = 5 ;','')
+        if interesting_code[-1] != OPEN_SPECIAL_BRACKET:
+            to_return += OPEN_SPECIAL_BRACKET
+        return to_return
+    # if not branch - we take the line
+    return ' '.join(interesting_code)
+
+
+# integrate the translated code into our output
+# we will use trivial way - strip each line and put it in its place with the correct indentation
+def integrate_code(translated):
+    integrated = [] # strip the first line
+    stripped_lines = [[0, translated[0][2].split('\n')[0]]]
+    for i in range(1, len(translated)): # strip all the lines to prepare them for the integration
+                                        # we need the indentation because we will use it to locate branches code
+        stripped_lines.append([translated[i][0], strip_line(translated[i][2])])
+    integrated = stripped_lines
+    # TODO - create integrated. only add indentation and '}' to close each branch (including the function itself)
+    return integrated
+
+
 if __name__ == '__main__':
     print_time('start')
     with open('/mnt/c/TransCoder/outputs/python/BINARY_SEARCH.py') as file:
@@ -78,4 +126,5 @@ if __name__ == '__main__':
     slices = slice_code(comfortable_code)
     slices = runable_slices(slices, ''.join(code))
     translated = translate_lines(False, slices)
+    integrated = integrate_code(translated)
     a = 5
