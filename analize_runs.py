c1043886f6829fe50f664a216756e9cceb8806fc
yuvalol <yuvalolshaker@gmail.com>
2020-09-24 20:40:38 +0300

    plot the analyzed data

--- analize_runs.py
+++ analize_runs.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 
 # returns a list with the elements that are in the first list and not the second one
 def in_first_list_and_not_second(first_list, second_list):
@@ -6,6 +7,44 @@ def in_first_list_and_not_second(first_list, second_list):
         if name not in second_list:
             to_return.append(name)
     return to_return
+
+
+# prints lines and conditions graphs
+def plot_both_amounts_graphs(results, save_name):
+    plot_amounts_graph(results, save_name, True)
+    plot_amounts_graph(results, save_name, False)
+
+
+
+# plot a graph of function lines number to condition number.
+def plot_amounts_graph(results, save_name, is_lines):
+    if is_lines:
+        counter_type = '_lines'
+        column = 2
+    else:
+        counter_type = '_conditions'
+        column = 3
+    y_axis = list(map(int, column_from_2dim_list(results, column)))
+
+    counters = []
+    for i in range(max(y_axis) + 1):
+        counters.append(y_axis.count(i))
+
+    plt.plot(range(max(y_axis) + 1), counters, 'b.')
+    plt.xlabel(counter_type[1:] + ' num')
+    plt.ylabel('amount')
+
+    plt.savefig('/mnt/c/TransCoder/outputs/figs/' + save_name + counter_type + '.png')
+    plt.close()
+
+# organizes line from file
+def organize_line(line):
+    splitted = line.split()
+    return splitted[0], splitted[6], splitted[2], splitted[4]
+
+
+def column_from_2dim_list(two_dim_list, column):
+    return list(map(lambda line: line[column], two_dim_list))
 
 if __name__ == '__main__':
     no_slice_path = '/mnt/c/TransCoder/outputs/no_slices_run/tests_run_check_java.txt'
@@ -18,21 +57,42 @@ if __name__ == '__main__':
         slices_lines = slices_file.readlines()
 
     # clean data to only results
-    no_slices_results = list(map(lambda line: (line.split()[0], line.split()[6]), no_slices_lines))
-    slices_results = list(map(lambda line: (line.split()[0], line.split()[6]), slices_lines))
+    no_slices_results = list(map(organize_line, no_slices_lines))
+    slices_results = list(map(organize_line, slices_lines))
 
-    # filter to perfect and didnt run
-    no_slices_perfect = list(map(lambda line: line[0], filter(lambda line: line[1] == '10', no_slices_results)))
-    no_slices_not_run = list(map(lambda line: line[0],filter(lambda line: line[1] == '-1', no_slices_results)))
-    slices_perfect = list(map(lambda line: line[0],filter(lambda line: line[1] == '10', slices_results)))
-    slices_not_run = list(map(lambda line: line[0],filter(lambda line: line[1] == '-1', slices_results)))
+    # filter to the different kinds:
+    # -1 - did not run (or infinite loop)
+    # 10 - perfect run - all tests are successes
+    # anything else - ran but with mistakes
+    no_slices_perfect_results = list(filter(lambda line: line[1] == '10', no_slices_results))
+    no_slices_not_run_results = list(filter(lambda line: line[1] == '-1', no_slices_results))
+    no_slices_medium_results = list(filter(lambda line: line[1] != '-1' and line[1] != '10', no_slices_results))
+    slices_perfect_results = list(filter(lambda line: line[1] == '10', slices_results))
+    slices_not_run_results = list(filter(lambda line: line[1] == '-1', slices_results))
+    slices_medium_results = list(filter(lambda line: line[1] != '-1' and line[1] != '10', slices_results))
+
+
+    # plot graphs that shows us functions length and conditions num to successes
+    plot_both_amounts_graphs(no_slices_perfect_results, 'no_slices_perfect_results')
+    plot_both_amounts_graphs(no_slices_not_run_results, 'no_slices_not_run_results')
+    plot_both_amounts_graphs(no_slices_medium_results, 'no_slices_medium_results')
+    plot_both_amounts_graphs(slices_perfect_results, 'slices_perfect_results')
+    plot_both_amounts_graphs(slices_not_run_results, 'slices_not_run_results')
+    plot_both_amounts_graphs(slices_medium_results, 'slices_medium_results')
+
+
+    # stay with the names
+    no_slices_perfect_names = column_from_2dim_list(no_slices_perfect_results, 0)
+    no_slices_not_run_names = column_from_2dim_list(no_slices_not_run_results, 0)
+    slices_perfect_names = column_from_2dim_list(slices_perfect_results, 0)
+    slices_not_run_names = column_from_2dim_list(slices_not_run_results, 0)
 
     # find differences
-    slices_added_perfect = in_first_list_and_not_second(slices_perfect, no_slices_perfect)
-    slices_destroyed_perfect = in_first_list_and_not_second(no_slices_perfect, slices_perfect)
+    slices_added_perfect = in_first_list_and_not_second(slices_perfect_names, no_slices_perfect_names)
+    slices_destroyed_perfect = in_first_list_and_not_second(no_slices_perfect_names, slices_perfect_names)
 
-    slices_added_no_run = in_first_list_and_not_second(slices_not_run, no_slices_not_run)
-    slices_destroyed_no_run = in_first_list_and_not_second(no_slices_not_run, slices_not_run)
+    slices_added_no_run = in_first_list_and_not_second(slices_not_run_names, no_slices_not_run_names)
+    slices_destroyed_no_run = in_first_list_and_not_second(no_slices_not_run_names, slices_not_run_names)
 
     with open('/mnt/c/TransCoder/outputs/comparison.txt', 'w') as comparison:
         comparison.write('added perfect: \n\n')
