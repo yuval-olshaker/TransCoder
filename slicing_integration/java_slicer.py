36429c9a7ae8680c94cbec95e208500d0a8694e4
yuvalol <yuvalolshaker@gmail.com>
2020-09-29 00:40:05 +0300

    work without indentation on java (we really do not need them). fix many bugs while organizing the java code for slicing

--- slicing_integration/java_slicer.py
+++ slicing_integration/java_slicer.py
@@ -3,12 +3,19 @@ import utils
 
 
 # return the line number of the ending of the statement (we know it by closing the bracket - java syntax)
-# TODO: need to count '(' and ')'. because inside a condition can be ().
 def find_end_statement_line(code_splitted, i):
+    counter = 0
     while i < len(code_splitted):
-        if utils.CLOSE_BRACKET in code_splitted[i]:
-            return i
+        if utils.CLOSE_BRACKET in code_splitted[i] or utils.OPEN_BRACKET in code_splitted[i]:
+            for word in code_splitted[i].split():
+                if word == utils.OPEN_BRACKET:
+                    counter += 1
+                elif word == utils.CLOSE_BRACKET:
+                    counter -= 1
+                    if counter == 0:
+                        return i
         i += 1
+
 
 
 # deletes double spaces
@@ -17,43 +24,67 @@ def no_double_spaces(combined):
         combined = combined.replace(utils.SPACE + utils.SPACE, utils.SPACE)
     return combined
 
-def handle_annoying_branch(comfortable_code, first_word, indentation, splitted, code_splitted, i):
-    second_word = splitted[indentation * utils.JAVA_INDENTATION + 1]
-    # else is handled alone, else if is like if statement
-    if first_word == utils.ELSE_STATEMENT and second_word != utils.IF_STATEMENT:
-        splitted_no_else = splitted[indentation * utils.JAVA_INDENTATION + 1:] # the line without the else
-        comfortable_code.append(# the else itself is a line
-            [first_word, indentation, utils.ELSE_STATEMENT + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
 
+# finds where the declaration ends (close the bracket)
+def find_end_statement_index(combined):
+    i = combined.index(utils.OPEN_BRACKET)
+    counter = 0
+    while i < len(combined):
+        if combined[i] == utils.OPEN_BRACKET:
+            counter += 1
+        elif combined[i] == utils.CLOSE_BRACKET:
+            counter -= 1
+            if counter == 0:
+                return i
+        i += 1
+
+
+
+# handles the annoying branches. the ones that has no { at the end of declaration line.
+# written badly. ordering java is a bit annoying. may need to work on making this better
+def handle_annoying_branch(comfortable_code, first_word, splitted, code_splitted, i):
+    second_word = splitted[1]
+    # else is handled alone, else if is like if statement
+    if first_word == utils.ELSE_STATEMENT and splitted[1] != utils.IF_STATEMENT:
+        splitted_no_else = splitted[1:] # the line without the else
+        comfortable_code.append(# the else itself is a line
+            [first_word, utils.ELSE_STATEMENT + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
         # add the inside - if branch we need to handle it properly - special line
-        if splitted[1 + indentation * utils.JAVA_INDENTATION] in utils.BRANCH_STATEMENTS:
+        if splitted[1] in utils.BRANCH_STATEMENTS:
             # we change the line in the code splitted line to use it. it is ok because we only go forward
             code_splitted[i] = utils.SPACE.join(splitted_no_else)
             comfortable_code, i = handle_annoying_branch(comfortable_code,
-                                        splitted[1 + indentation * utils.JAVA_INDENTATION], # new first word
-                                                         indentation, splitted_no_else, code_splitted, i)
+                                        splitted[1], splitted_no_else, code_splitted, i)
         else: # regular line, then first word is regular and we append the line (after the else)
-            comfortable_code.append([utils.REGULAR, indentation,utils.SPACE.join(splitted_no_else)])
+            comfortable_code.append([utils.REGULAR, utils.SPACE.join(splitted_no_else)])
         # because this situation only happens with 1 line and no '}' we add it as well
-        comfortable_code.append([utils.REGULAR, indentation, utils.CLOSE_SPECIAL_BRACKET])
+        comfortable_code.append([utils.REGULAR, utils.CLOSE_SPECIAL_BRACKET])
     else:
         end_statement_line = find_end_statement_line(code_splitted, i)  # finds the line that the statement ends
         combined = utils.SPACE.join(code_splitted[i:end_statement_line + 1])  # one line for statement
-        combined = no_double_spaces(combined)
+        combined = no_double_spaces(combined).split()
         i = end_statement_line  # advance i (only if it changed)
         # checks if there is code in that line
-        statement_ends = combined.index(utils.CLOSE_BRACKET)
-        if combined.endswith(utils.OPEN_SPECIAL_BRACKET):
-            comfortable_code.append([first_word, indentation, combined])
+        statement_ends = find_end_statement_index(combined)
+        if combined[-1] == utils.OPEN_SPECIAL_BRACKET:
+            comfortable_code.append([first_word, utils.SPACE.join(combined)])
         elif statement_ends == len(combined) - 1:  # add the line - there is no code inside the branch (here)
-            comfortable_code.append([first_word, indentation, combined + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
+            comfortable_code.append([first_word,
+                                     utils.SPACE.join(combined) + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
         else:  # adds the declaration and the inside of branch in 2 separate lines
             comfortable_code.append(
-                [first_word, indentation, combined[:statement_ends + 1] + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
-            # the first word is regular because branch in branch requires '{' - (i think)
-            comfortable_code.append([utils.REGULAR, indentation, combined[statement_ends + 1:]])
+                [first_word,
+                 utils.SPACE.join(combined[:statement_ends + 1]) + utils.SPACE + utils.OPEN_SPECIAL_BRACKET])
+            new_first_word = combined[statement_ends + 1]
+            if new_first_word in utils.BRANCH_STATEMENTS:
+                only_in_branch = combined[statement_ends + 1:]  # needs to continue with the inside
+                code_splitted[i] = utils.SPACE.join(only_in_branch) # like inside else - we destroy the origin line
+                comfortable_code, i = handle_annoying_branch(comfortable_code,
+                                                             new_first_word, only_in_branch, code_splitted, i)
+            else:  # the first word is regular (no branch)
+                comfortable_code.append([utils.REGULAR, utils.SPACE.join(combined[statement_ends + 1:])])
             # because this situation only happens with 1 line and no '}' we add it as well
-            comfortable_code.append([utils.REGULAR, indentation, utils.CLOSE_SPECIAL_BRACKET])
+            comfortable_code.append([utils.REGULAR, utils.CLOSE_SPECIAL_BRACKET])
     return comfortable_code, i
 
 class JavaSlicer(SlicingClass):
@@ -75,18 +106,16 @@ class JavaSlicer(SlicingClass):
         comfortable_code = []
         i = 0
         while i < len(code_splitted): # run over all the lines of the code
-            line = code_splitted[i]
+            line = code_splitted[i].strip()
             splitted = line.split(utils.SPACE)
-            indentation = self.get_indentation(splitted, True)
-            first_word = splitted[indentation * utils.JAVA_INDENTATION] # extract first word (has more meaning)
+            first_word = splitted[0] # extract first word (has more meaning)
             is_branch = first_word in utils.BRANCH_STATEMENTS
             if is_branch and splitted[-1] != utils.OPEN_SPECIAL_BRACKET: # annoying situation with branches
-                comfortable_code, i = handle_annoying_branch(comfortable_code, first_word, indentation,splitted, code_splitted, i)
+                comfortable_code, i = handle_annoying_branch(comfortable_code, first_word,splitted, code_splitted, i)
             else:
                 if not is_branch:
                     first_word = utils.REGULAR
-                comfortable_code.append(
-                    [first_word, indentation, utils.SPACE.join(splitted[indentation * utils.JAVA_INDENTATION:])])
+                comfortable_code.append([first_word, utils.SPACE.join(splitted)])
             i += 1
 
         return comfortable_code
