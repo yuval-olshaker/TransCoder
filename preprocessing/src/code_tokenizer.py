d1f40615620d4a4fa2ab60ab7ee2c597a7926ce4
yuvalol <yuvalolshaker@gmail.com>
2021-01-21 13:43:22 +0200

    running pretraining

--- preprocessing/src/code_tokenizer.py
+++ preprocessing/src/code_tokenizer.py
@@ -23,7 +23,7 @@ from preprocessing.src.timeout import timeout, TimeoutError
 
 TOK_NO_SPACE_BEFORE = {',', ';'}
 # clang.cindex.Config.set_library_path('/usr/lib/llvm-7/lib/')
-clang.cindex.Config.set_library_path('/home/ubuntu/anaconda3/envs/aws_neuron_pytorch_p36/lib/')
+# clang.cindex.Config.set_library_path('/home/ubuntu/anaconda3/envs/aws_neuron_pytorch_p36/lib/')
 STRINGS_AND_COMMENTS_TOKEN_KINDS = {TokenKind.LITERAL, TokenKind.COMMENT}
 logging.basicConfig(
     filename='timeout_cpp_tokenizer_examples.log', level=logging.DEBUG)
