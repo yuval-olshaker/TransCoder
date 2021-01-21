5843586d7e646d7424b4250829f9f7aab3ebd2e5
yuvalol <yuvalolshaker@gmail.com>
2021-01-21 13:12:54 +0200

    running pretraining

--- preprocessing/run_exp.py
+++ preprocessing/run_exp.py
@@ -1,7 +1,8 @@
 import os
 from os.path import isfile, join
 langs = ['c', 'wat']
-
+# Transcoder_path = '/mnt/c/TransCoder/TransCoder/'
+Transcoder_path = '/home/ubuntu/wasm_decompiler/TransCoder/'
 
 def get_all_files(path):
     return [f for f in os.listdir(path) if isfile(join(path, f))]
@@ -11,16 +12,16 @@ if __name__ == '__main__':
     long = True
     if long:
         for lang in langs:
-            path = '/mnt/c/TransCoder/data/WASM/' + lang + '/'
+            path = Transcoder_path + 'data/WASM/' + lang + '/'
             for file_name in get_all_files(path):
                 if not file_name.endswith('corpus_small_fixed'):
                     os.system('rm ' + path + file_name)
         print('killed files')
-        os.system('rm -rf /mnt/c/TransCoder/data/WASM/c-wat-')
+        os.system('rm -rf ' + Transcoder_path + 'data/WASM/c-wat-')
         print('killed combined')
 
         print('start to_json')
-        os.system('python3 /mnt/c/TransCoder/preprocessing/to_json.py')
+        os.system('python3 ' + Transcoder_path + 'preprocessing/to_json.py')
 
     print('start training')
-    os.system('python3 -m preprocessing.preprocess  /mnt/c/TransCoder/data/WASM --lang1 c --lang2 wat --bpe_train_size 0 --test_size 100 --local True')
+    os.system('python3 -m preprocessing.preprocess ' + Transcoder_path + 'data/WASM --lang1 c --lang2 wat --bpe_train_size 0 --test_size 100 --local True')
