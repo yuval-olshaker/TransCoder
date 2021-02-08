0af919e9665c82dcd2d80aa3bf2dd4873db0a36d
yuvalol <yuvalolshaker@gmail.com>
2021-02-08 11:16:53 +0200

    for x86 run

--- XLM/src/data/loader.py
+++ XLM/src/data/loader.py
@@ -22,6 +22,7 @@ def process_binarized(data, params):
     Process a binarized dataset and log main statistics.
     """
     dico = data['dico']
+    print(data['unk_words'])
     assert ((data['sentences'].dtype == np.uint16) and (len(dico) < 1 << 16) or
             (data['sentences'].dtype == np.int32) and (1 << 16 <= len(dico) < 1 << 31))
     logger.info("%i words (%i unique) in %i sentences. %i unknown words (%i unique) covering %.2f%% of the data." % (
