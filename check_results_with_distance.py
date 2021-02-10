03b44f018e7b3d7ef042d7f9b7decfae1a496c3c
yuvalol <yuvalolshaker@gmail.com>
2021-02-10 17:18:52 +0200

    check results

--- check_results_with_distance.py
+++ check_results_with_distance.py
@@ -2,42 +2,101 @@ import Levenshtein
 
 
 ref = '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/ref.wat_sa-c_sa.test.txt'
-double_translated0 = '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/hyp0.wat_sa-c_sa.test_beam0.txt'
-double_translated1 = '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/hyp0.wat_sa-c_sa.test_beam1.txt'
-double_translated2 = '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/hyp0.wat_sa-c_sa.test_beam2.txt'
-lean_translated0 = double_translated0.replace('double_stage','mt_ae_lean')
-lean_translated1 = double_translated1.replace('double_stage','mt_ae_lean')
-lean_translated2 = double_translated2.replace('double_stage','mt_ae_lean')
+wat_ref = '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/ref.c_sa-wat_sa.test.txt'
+double_translated_paths = list(map(lambda i: '/mnt/c/TransCoder/outputs/c-wat/double_stage/eval/hyp0.wat_sa-c_sa.test_beam' + str(i) + '.txt', range(25)))
+lean_translated_paths = list(map(lambda i: '/mnt/c/TransCoder/outputs/c-wat/mt_ae_lean/eval/hyp0.wat_sa-c_sa.test_beam' + str(i) + '.txt', range(3)))
+baseline_translated_paths = list(map(lambda i: '/mnt/c/TransCoder/outputs/c-wat/baseline_only_transformer_parallel/eval/hyp0.wat_sa-c_sa.test_beam' +str(i) + '.txt', range(3)))
+
+
+ref2 = '/mnt/c/TransCoder/outputs/c-wat/check/refs.txt'
+trans2 = '/mnt/c/TransCoder/outputs/c-wat/check/trans.txt'
+
+output_path = '/mnt/c/TransCoder/outputs/c-wat/'
+double_succ = output_path + 'double_succ.txt'
+lean_succ = output_path + 'lean_succ.txt'
 
 def distance(or_line, tested_line):
     return Levenshtein.distance(or_line, tested_line)
 
 def return_lines(path):
     with open(path) as f:
-        lines = f.readlines()
+        lines = list(map(lambda line: line.replace('void ','').replace('static ','') ,f.readlines()))
     return lines
 
+def check():
+    ref2_lines = return_lines(ref2)
+    trans2_lines = return_lines(trans2)
+    iss = []
+    for i, ref_line in enumerate(ref2_lines):
+        if ref_line == trans2_lines[i]:
+            iss.append(i)
+    print(iss)
+    print(len(iss))
+    exit(1)
+
+def check_succ(succ_path, only_list, ref_lines, wat_lines, translated0_lines, translated1_lines, translated2_lines):
+    with open(succ_path, 'w') as f:
+        for i in only_list:
+            f.write(str(i) + '\n')
+            f.write(ref_lines[i])
+            f.write(wat_lines[i])
+            f.write(translated0_lines[i])
+            f.write(translated1_lines[i])
+            f.write(translated2_lines[i])
+            f.write('\n\n')
+
 if __name__ == "__main__":
+    # check()
     ref_lines = return_lines(ref)
+    wat_lines = return_lines(wat_ref)
 
-    double_translated0_lines = return_lines(double_translated0)
-    double_translated1_lines = return_lines(double_translated1)
-    double_translated2_lines = return_lines(double_translated2)
-
-    lean_translated0_lines = return_lines(lean_translated0)
-    lean_translated1_lines = return_lines(lean_translated1)
-    lean_translated2_lines = return_lines(lean_translated2)
+    double_translated_lines = list(map(return_lines, double_translated_paths))
+    lean_translated_lines = list(map(return_lines, lean_translated_paths))
+    baseline_translated_lines = list(map(return_lines, baseline_translated_paths))
 
     results_double = []
     results_lean = []
+    results_base = []
 
+    print(len(ref_lines))
     for i, ref_line in enumerate(ref_lines):
-        d_double = min(distance(ref_line, double_translated0_lines[i]), distance(ref_line, double_translated1_lines[i]), distance(ref_line, double_translated2_lines[i]))
-        d_lean = min(distance(ref_line, lean_translated0_lines[i]), distance(ref_line, lean_translated1_lines[i]), distance(ref_line, lean_translated2_lines[i]))
+        d_double = min(list(map(lambda lines: distance(ref_line, lines[i]), double_translated_lines)))
+        d_lean = min(list(map(lambda lines: distance(ref_line, lines[i]), lean_translated_lines)))
+        d_base = min(list(map(lambda lines: distance(ref_line, lines[i]), baseline_translated_lines)))
+
         results_double.append(d_double)
         results_lean.append(d_lean)
+        results_base.append(d_base)
 
 
-    print('double model total: ' + str(sum(results_double)) + ' correct num: ' + str(len(list(filter(lambda x: x ==0, results_double)))))
+
+    zeros_d = []
+    zeros_l = []
+    zeros_b = []
+    for i, res in enumerate(results_double):
+        if res == 0:
+            zeros_d.append(i)
+        if results_lean[i] == 0:
+            zeros_l.append(i)
+        if results_base[i] == 0:
+            zeros_b.append(i)
+
+    only_d = sorted(set(zeros_d).difference(set(zeros_l)))
+    only_l = sorted(set(zeros_l).difference(set(zeros_d)))
+
+    print('only_d: zise - ' + str(len(only_d)) + ' list - ' +  str(only_d))
+    print('only_d: zise - ' + str(len(only_l)) + ' list - ' + str(only_l))
+
+    check_succ(double_succ, only_d, ref_lines, wat_lines, lean_translated_lines[0], lean_translated_lines[1], lean_translated_lines[2])
+    check_succ(lean_succ, only_l, ref_lines, wat_lines, double_translated_lines[0], double_translated_lines[1], double_translated_lines[2])
+
+    print(zeros_d)
+    print(zeros_l)
+    print(zeros_b)
+
+
+    print()
+    print()
+    print('double model total: ' + str(sum(results_double)) + ' correct num: ' + str(len(list(filter(lambda x: x == 0, results_double)))))
     print('one model total: ' + str(sum(results_lean)) + ' correct num: ' + str(len(list(filter(lambda x: x ==0, results_lean)))))
-    a = 5
+    print('base model total: ' + str(sum(results_base)) + ' correct num: ' + str(len(list(filter(lambda x: x == 0, results_base)))))
