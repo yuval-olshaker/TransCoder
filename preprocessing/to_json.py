0d90f87b397b11710f6779d0ebb830fafd08458b
yuvalol <yuvalolshaker@gmail.com>
2021-03-21 17:59:05 +0200

    fix bug

--- preprocessing/to_json.py
+++ preprocessing/to_json.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
         for i, line in enumerate(lines):
             try:
                 x = json.loads(line)
+                print(i)
                 new_lines.append(line)
             except:
                 continue
