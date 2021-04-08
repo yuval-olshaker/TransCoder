d8a1d5bd48980ec7aeec311f47feae070d5bcd88
yuvalol <yuvalolshaker@gmail.com>
2021-04-08 17:24:13 +0300

    full run separated

--- preprocessing/run_exp.py
+++ preprocessing/run_exp.py
@@ -1,7 +1,7 @@
 import os
 from os.path import isfile, join
 langs = ['c', 'wat']
-data_path = 'data/WASM_all/'
+data_path = 'data/WASM_full/'
 # Transcoder_path = '/mnt/c/TransCoder/'
 Transcoder_path = '/home/ubuntu/wasm_decompiler/TransCoder/'
 
