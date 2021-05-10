dfba6d3390322d16a9a193fb64c539b79247d224
yuvalol <yuvalolshaker@gmail.com>
2021-05-10 15:13:18 +0300

    fix

--- preprocessing/src/utils.py
+++ preprocessing/src/utils.py
@@ -186,7 +186,7 @@ def learn_bpe_file(file_path, ncodes, codes):
     assert Path(
         f"{str(codes)}").is_file, f"failed to output codes, cannot find codes {str(codes)}"
     if ncodes > 50000: #remember, numbers here
-        codes.write_text(''.join(head(codes, 2887)), encoding='utf-8')
+        codes.write_text(''.join(head(codes, 279)), encoding='utf-8')
 
 
 def get_vocab_file(file_path, vocab):
